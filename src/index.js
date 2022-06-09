9691102dbb8c1090db9e5e9cb2719d9e0a4faa89
Quintin <zelotbe@gmail.com>
2022-06-09 18:37:34 +0200

    Code cleanup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-//BOOTSTRAP TOEVOEGEN
 import * as bootstrap from 'bootstrap';
 import './scss/app.scss';
-
 
 // Import from "@inrupt/solid-client-authn-browser"
 import {
@@ -30,7 +28,7 @@ import {
 
 import { VCARD, RDF, AS, SCHEMA_INRUPT } from "@inrupt/vocab-common-rdf";
 
-// Meeste DOM elementen die we gaan gebruiken.
+// DOM elements that we are gonna use
 const main = document.querySelector("main");
 const welkom = document.querySelector("#welkom");
 const error = document.querySelector("#error");
@@ -41,16 +39,15 @@ const buttonLogin = document.querySelector("#btnLogin");
 const buttonLogout = document.querySelector("#btnLogout");
 const buttonUpdate = document.querySelector("#btnUpdate");
 const status = document.querySelector("#status");
-let myReadingList;
+let list;
 
-
+//HIDING VARIOUS ELEMENTS
 main.classList.add("d-none");
 welkom.classList.add("d-none");
 buttonUpdate.setAttribute("disabled", "disabled");
 
-// 1a. Start Login Process. Call login() function.
+// LOGIN
 function loginToSelectedIdP() {
-
     return login({
         oidcIssuer: "https://solidcommunity.net",
         redirectUrl: window.location.href,
@@ -58,8 +55,7 @@ function loginToSelectedIdP() {
     });
 }
 
-// 1b. Login Redirect. Call handleIncomingRedirect() function.
-// When redirected after login, finish the process by retrieving session information.
+//AFTER LOGIN -> READ DATA
 async function handleRedirectAfterLogin() {
     const session = getDefaultSession();
     await handleIncomingRedirect();
@@ -73,19 +69,14 @@ async function handleRedirectAfterLogin() {
     }
 }
 
-// The example has the login redirect back to the index.html.
-// This calls the function to process login information.
-// If the function is called when not part of the login redirect, the function is a no-op.
 handleRedirectAfterLogin();
 
-// 2. Get Pod(s) associated with the WebID
+// RETRIEVE POD(S) FROM THE USER
 async function getMyPods() {
     const session = getDefaultSession();
     const webID = session.info.webId;
 
     const mypods = await getPodUrlAll(webID, { fetch: fetch });
-
-    // Update the page with the retrieved values.
 
     mypods.forEach((mypod) => {
         let podOption = document.createElement("option");
@@ -95,9 +86,12 @@ async function getMyPods() {
     });
 }
 
+//READ PROFILE FOR NAME
 async function readProfile() {
     const session = getDefaultSession();
     const webID = session.info.webId;
+
+    //CHECK IF THE WEBID IS VALID
     try {
         new URL(webID);
     } catch (_) {
@@ -105,11 +99,9 @@ async function readProfile() {
         return false;
     }
 
+    //URL CLEANUP (REMOVE THINGS AFTER URL)
     const profileDocumentUrl = new URL(webID);
     profileDocumentUrl.hash = "";
-
-    // Profile is public data; i.e., you do not need to be logged in to read the data.
-    // For illustrative purposes, shows both an authenticated and non-authenticated reads.
 
     let myDataset;
     try {
@@ -127,80 +119,75 @@ async function readProfile() {
 
     const formattedName = getStringNoLocale(profile, VCARD.fn);
 
-    // Update the page with the retrieved values.
+    // UPDATE HTML FOR NAME
     welkom.classList.remove("d-none");
     welkom.innerHTML += ` <a href="${webID}" target="_blank">${formattedName}</a>`;
 }
 
-// 3. Create the Reading List
+// CREATE TODO LIST
 async function createList() {
     status.textContent = "";
     const SELECTED_POD = document.getElementById("select-pod").value;
 
-    // For simplicity and brevity, this tutorial hardcodes the  SolidDataset URL.
-    // In practice, you should add in your profile a link to this resource
-    // such that applications can follow to find your list.
     const readingListUrl = `${SELECTED_POD}workshop/ToDo/studyList`;
 
     let todoItem = todoList.value.split("\n");
 
-    // Fetch or create a new reading list.
-
+    //TRY TO GET THE LIST FROM THE POD
     try {
-        // Attempt to retrieve the reading list in case it already exists.
-        //myReadingList = await getSolidDataset(readingListUrl, { fetch: fetch });
-        // Clear the list to override the whole list
-        let items = getThingAll(myReadingList);
+        // CLEARING THE LIST TO OVERRIDE
+        let items = getThingAll(list);
         items.forEach((item) => {
             console.log(item)
-            myReadingList = removeThing(myReadingList, item);
+            list = removeThing(list, item);
         });
     } catch (error) {
         if (typeof error.statusCode === "number" && error.statusCode === 404) {
-            // if not found, create a new SolidDataset (i.e., the reading list)
-            myReadingList = createSolidDataset();
+            // IF THE LIST DOES NOT EXIST THEN CREATE A NEW ONE
+            list = createSolidDataset();
         } else {
             setError(error.message);
             console.error(error.message);
         }
     }
 
-    // Add todoItem to the Dataset
+    // ADD ITEMS TO THE LIST
     let i = 0;
     todoItem.forEach((title) => {
         if (title.trim() !== "") {
             let item = createThing({ name: "title" + i });
             item = addUrl(item, RDF.type, AS.Article);
             item = addStringNoLocale(item, SCHEMA_INRUPT.name, title);
-            myReadingList = setThing(myReadingList, item);
-            console.log(myReadingList);
+            list = setThing(list, item);
+            console.log(list);
             i++;
         }
     });
+
+    //TRY TO SAVE THE LIST
     try {
-        // Save the SolidDataset
         let savedReadingList = await saveSolidDatasetAt(
-          readingListUrl,
-          myReadingList,
-          { fetch: fetch }
+            readingListUrl,
+            list,
+            { fetch: fetch }
         );
         status.textContent = "Lijst bijgewerkt";
     } catch (error) {
         setError(error);
     }
-    
 }
-
+// GET THE TODO LIST
 async function getTodoList() {
     const SELECTED_POD = document.getElementById("select-pod").value;
 
+    //SELECT VALUE CHECK
     if (SELECTED_POD !== "") {
         const readingListUrl = `${SELECTED_POD}workshop/ToDo/studyList`;
-        myReadingList = await getSolidDataset(readingListUrl, { fetch: fetch });
+        list = await getSolidDataset(readingListUrl, { fetch: fetch });
 
         let savedReadingList = await saveSolidDatasetAt(
             readingListUrl,
-            myReadingList,
+            list,
             { fetch: fetch }
         );
 
